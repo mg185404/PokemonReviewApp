8a52fd6268f0fde8c65097fe2c1e7c2fa082305c
Milos Gunjic <mg185404@ncr.com>
2023-11-23 16:10:09 +0100

    ReviewerController read methods

---
+++ PokemonReviewApp/Controllers/ReviewerController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Repository;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] 
+    public class ReviewerController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly IReviewerRepository _reviewerRepository;
+
+        public ReviewerController(IMapper mapper, IReviewerRepository reviewerRepository)
+        {
+            _mapper = mapper;
+            _reviewerRepository = reviewerRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetReviewers()
+        {
+            var reviewers = _mapper.Map<List<ReviewerDto>>(_reviewerRepository.GetReviewers());
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(reviewers);
+        }
+
+        [HttpGet("reviewerId")]
+        public IActionResult GetReviewer(int reviewerId)
+        {
+            if (!_reviewerRepository.ReviewerExists(reviewerId))
+                return NotFound();
+
+            var reviewer = _mapper.Map<ReviewerDto>(_reviewerRepository.GetReviewer(reviewerId));
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            return Ok(reviewer);
+        }
+
+        [HttpGet("{reviewerId}/reviews")]
+        public IActionResult GetReviewsByAReviewer(int reviewerId)
+        {
+            if (!_reviewerRepository.ReviewerExists(reviewerId))
+                return NotFound();
+
+            var reviews = _mapper.Map<List<ReviewDto>>(_reviewerRepository.GetReviewsByReviewer(reviewerId));
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(reviews);
+        }
+    }
+}
