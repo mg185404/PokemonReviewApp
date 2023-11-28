d922d8f3583802d0997d1e53461ba3befadf6b8a
Milos Gunjic <mg185404@ncr.com>
2023-11-28 15:14:36 +0100

    added owner repository

--- PokemonReviewApp/Repository/OwnerRepository.cs
+++ PokemonReviewApp/Repository/OwnerRepository.cs
@@ -8,12 +8,16 @@ namespace PokemonReviewApp.Repository
     public class OwnerRepository : IOwnerRepository
     {
         private readonly DataContext _context;
-        private readonly IMapper _mapper;
 
         public OwnerRepository(IMapper mapper, DataContext context)
         {
-            _mapper = mapper;
             _context = context;
+        }
+
+        public bool CreateOwner(Owner owner)
+        {
+            _context.Owners.Add(owner); 
+            return Save();
         }
 
         public Owner? GetOwner(int id)
@@ -45,6 +49,12 @@ namespace PokemonReviewApp.Repository
         public bool OwnerExists(int ownerId)
         {
             return _context.Owners.Any(o => o.Id == ownerId);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
         }
     }
 }
