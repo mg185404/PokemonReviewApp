466e163965604fdda1b1867dd3db8fb7ff6551af
Milos Gunjic <mg185404@ncr.com>
2023-11-29 12:24:47 +0100

    added post method for reviewers

--- PokemonReviewApp/Controllers/ReviewerController.cs
+++ PokemonReviewApp/Controllers/ReviewerController.cs
@@ -3,12 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 using PokemonReviewApp.Data;
 using PokemonReviewApp.Dto;
 using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
 using PokemonReviewApp.Repository;
 
 namespace PokemonReviewApp.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController] 
+    [ApiController]
     public class ReviewerController : Controller
     {
         private readonly IMapper _mapper;
@@ -57,6 +58,35 @@ namespace PokemonReviewApp.Controllers
                 return BadRequest(ModelState);
 
             return Ok(reviews);
+        }
+
+        [HttpPost]
+        public IActionResult CreateReviewer([FromBody] ReviewerDto reviewerDto)
+        {
+            if (reviewerDto == null)
+                return BadRequest(ModelState);
+
+            var reviewer = _reviewerRepository.GetReviewers()
+                .Where(r => r.LastName == reviewerDto.LastName).FirstOrDefault();
+
+            if (reviewer != null)
+            {
+                ModelState.AddModelError("", "Reviewer already exists");
+                return StatusCode(422, ModelState);
+            }
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var reviewerMap = _mapper.Map<Reviewer>(reviewerDto);
+
+            if (!_reviewerRepository.CreateReviewer(reviewerMap))
+            {
+                ModelState.AddModelError("", "Something went wrong while saving data");
+                return StatusCode(500, ModelState);
+            }
+
+            return Ok("Successfuly created");
         }
     }
 }
--- PokemonReviewApp/Repository/ReviewerRepository.cs
+++ PokemonReviewApp/Repository/ReviewerRepository.cs
@@ -13,6 +13,12 @@ namespace PokemonReviewApp.Repository
             _context = context;
         }
 
+        public bool CreateReviewer(Reviewer reviewer)
+        {
+            _context.Reviewers.Add(reviewer);
+            return Save();
+        }
+
         public Reviewer? GetReviewer(int id)
         {
             return _context.Reviewers.Where(r => r.Id == id).Include(e => e.Reviews).FirstOrDefault();
@@ -31,6 +37,12 @@ namespace PokemonReviewApp.Repository
         public bool ReviewerExists(int reviewerId)
         {
             return _context.Reviewers.Any(r => r.Id == reviewerId);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
         }
     }
 }
