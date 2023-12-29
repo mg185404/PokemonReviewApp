e55b3d243b3601e72ed16c967521e23b41d7e150
Milos Gunjic <mg185404@ncr.com>
2023-12-29 15:45:37 +0100

    clean pokemon

--- PokemonReviewApp.Tests/Controllers/PokemonControllerTests.cs
+++ PokemonReviewApp.Tests/Controllers/PokemonControllerTests.cs
@@ -1,13 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using FakeItEasy;
+using PokemonReviewApp.Interfaces;
 
 namespace PokemonReviewApp.Tests.Controllers
 {
     public class PokemonControllerTests
     {
+        private readonly IPokemonRepository _pokemonRepository;
+        private readonly IReviewRepository _reviewRepository;
+        private readonly IMapper _mapper;
+        public PokemonControllerTests(IReviewRepository reviewRepository, IMapper mapper)
+        {
+            _pokemonRepository = A.Fake<IPokemonRepository>();
+            _reviewRepository = A.Fake<IReviewRepository>();
+            _mapper = A.Fake<Mapper>(); 
+        }
 
+        [Fact]
+        public void PokemonController_GetPokemons_ReturnOK()
+        {
+
+        }
     }
 }
